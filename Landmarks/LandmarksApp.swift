dc26ee98985ac332471ce3b2ae8fe811070906f6
Sophie Haber <SHaber@lcs.on.ca>
2024-01-29 11:09:36 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sophie Haber on 29.01.24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
