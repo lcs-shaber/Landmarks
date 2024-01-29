6493f8729e2c2922d308af16791af95fac26784a
Sophie Haber <SHaber@lcs.on.ca>
2024-01-29 11:12:46 -0500

    Created detail view and organized project by creating Views group.

---
+++ Landmarks/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Landmarks
+//
+//  Created by Sophie Haber on 29.01.24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
