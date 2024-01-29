58e5fe0577953e43be90face27e029a5d79b557b
Sophie Haber <SHaber@lcs.on.ca>
2024-01-29 11:55:40 -0500

    Added a gallery item view to use later inside a scrolling grid.

--- Landmarks/Views/ListView.swift
+++ Landmarks/Views/ListView.swift
@@ -22,7 +22,6 @@ struct ListView: View {
                 
             }
             .navigationTitle("Landmarks")
-            .navigationTitle("Landmarks")
                         
         }
         
